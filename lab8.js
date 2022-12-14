a1378daafd322b21a1a8b5ff423d80bbd959b3ef
Руссу Кирилл <kirillrussu708@gmail.com>
2022-12-14 09:45:03 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -4,4 +4,14 @@ function snowDate(params) {
     out.innerHTML = today.toLocaleDateString();
 }
 
+function showDaysCount(params) {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.ceil(daysCount); // округляяем в меньшую сторону
+    result.innerHTML = 'С даты рождения прошло дней: ' + daysCount;
+}
+
 window.addEventListener('load', snowDate);
