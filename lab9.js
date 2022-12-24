78e6c613b41048c6b8098c0f09c108d2c85fdc0e
Superpatsanchik <114040402+Superpatsanchik@users.noreply.github.com>
2022-12-24 06:33:46 +0000

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -4,9 +4,32 @@ function clickMe(button) {
         else
             button.innerHTML = 'Щелчок';
 }
+
 function clickMe2(button) {
     if(button.innerHTML == 'Кирилл')
         button.innerHTML = 'Руссу';
         else
             button.innerHTML = 'Кирилл';
+}
+
+function knopka(button) {
+    if(button.innerHTML == 'Нажми меня') {
+        button.innerHTML = 'Ещё раз нажми';
+        button.style.color = 'red';
+    }
+        
+    else if(button.innerHTML == 'Ещё раз нажми') {
+        button.innerHTML = 'Ещё';  
+        button.style.color = 'blue';
+    }
+         
+    else if(button.innerHTML == 'Ещё') {
+        button.innerHTML = 'Начать сначала';   
+        button.style.color = 'pink';
+    }
+        
+    else {
+        button.innerHTML = 'Нажми меня' 
+        button.style.color = 'violet'; 
+    }
 }
